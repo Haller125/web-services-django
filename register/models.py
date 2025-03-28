00e10d9dd5ea5113f9b8dca2447ed56a0d3b6ee8
haller125 <erokkabash1@gmail.com>
2025-03-28 20:10:32 +0500

    models added

--- register/models.py
+++ register/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    CurrencyChoices = [
+        ('USD', 'USD'),
+        ('EUR', 'EUR'),
+        ('GBP', 'GBP'),
+    ]
+
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    currency = models.CharField(choices=CurrencyChoices, max_length=3)
+    balance = models.FloatField()
